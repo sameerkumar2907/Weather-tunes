f6a343064d7ee7c3d773d98fede6fcbba733cc05
Sameer Kumar <sameerkumar2907@gmail.com>
2021-02-18 13:31:07 +0530

    Resolving homepage error

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exacy path="/Weather-tunes" component={Home} />
         <Route exact path="/form" component={Form} />
         <Route exact path="/listen" component={Listen} />
         <Route exact path="/about" component={About} />
